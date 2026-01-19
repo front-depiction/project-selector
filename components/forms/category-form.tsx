bb820eb2c66a8f650ae9e12f556c60ae5b052e18
JackOUT <jack.bikar@gmail.com>
2026-01-19 23:22:03 +0100

    Fixed ts error

--- components/forms/category-form.tsx
+++ components/forms/category-form.tsx
@@ -27,7 +27,7 @@ const formSchema = z.object({
     name: z.string().min(1, "Category name is required").min(2, "Category name must be at least 2 characters"),
     description: z.string().optional(),
     criterionType: z.enum(["prerequisite", "minimize", "pull"], {
-        required_error: "Please select how this category should be used",
+        message: "Please select how this category should be used"
     }),
     minRatio: z.number().min(0).max(100).optional(),
     target: z.number().min(0).max(100).optional(),
